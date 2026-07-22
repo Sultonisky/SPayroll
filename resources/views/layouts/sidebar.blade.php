f063aa6d6c82b0b69bc6149b1288801fa5abac66
sultonisky <muhsultonipml111@gmail.com>
2026-07-22 18:01:23 +0700

    refactor: move menu sidebar bonuses into section master data

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,9 +8,11 @@
                 <i class="fas fa-calculator" style="font-size: 1.5rem; color: var(--cui-primary);"></i>
             </div>
         </div>
-        <button class="btn-close d-lg-none" type="button" data-coreui-dismiss="offcanvas" data-coreui-theme="dark" aria-label="Close" onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()"></button>
+        <button class="btn-close d-lg-none" type="button" data-coreui-dismiss="offcanvas" data-coreui-theme="dark"
+            aria-label="Close"
+            onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()"></button>
     </div>
-    
+
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar>
         {{-- Dashboard --}}
         <li class="nav-item">
@@ -54,6 +56,14 @@
             </a>
         </li>
 
+        {{-- BONUS --}}
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('bonuses.index') }}">
+                <i class="nav-icon fas fa-comments-dollar"></i>
+                Bonus
+            </a>
+        </li>
+
         {{-- ATTENDANCE NAV - Temporarily Disabled --}}
         {{-- <li class="nav-title">Attendance</li> --}}
         {{-- Import Attendance --}}
@@ -88,12 +98,6 @@
         <li class="nav-title">Payroll</li>
         <!-- Payrolls -->
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('bonuses.index') }}">
-                <i class="nav-icon fas fa-comments-dollar"></i>
-                Bonus
-            </a>
-        </li>
-        <li class="nav-item">
             <a class="nav-link" href="{{ route('payrolls.generate') }}">
                 <i class="nav-icon fas fa-money-check-alt"></i>
                 Payrolls Generate
@@ -112,7 +116,7 @@
             </a>
         </li>
     </ul>
-    
+
     <div class="sidebar-footer border-top d-none d-md-flex">
         <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
     </div>
